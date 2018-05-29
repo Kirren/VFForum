f7927c3dcd46d42e38be4595dbad64b4f6faa0e8
Helen <kirren@live.com>
2018-05-29 16:47:14 +0300

    Rearrange post layout

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -32,36 +32,42 @@
         </small>
       </p>
     </b-col>
-    <b-col cols="9" md="10" class="post-content">
-      <div>
-        <p>
-          <small>{{post.text}}</small>
-        </p>
-      </div>
-      <a href="#"
-         class="ml-auto"
-         title="Make a change">
-        <icon name="pencil-alt"
-              scale="2" class="text-warning"/>
-      </a>
-      <div class="reactions float-right text-right">
-        <ul class="list-inline">
-          <li class="list-inline-item">💡</li>
-          <li class="list-inline-item">❤</li>
-          <li class="list-inline-item">👎</li>
-          <li class="list-inline-item">👍</li>
-          <li class="list-inline-item">👌</li>
-        </ul>
-        <b-button size="sm" variant="outline-success">
-          <span class="emoji">❤</span>️ 3
-        </b-button>
-        <b-button size="sm" variant="outline-success" class="active-reaction">
-          <span class="emoji">👌️</span>️ 1
-        </b-button>
-        <b-button size="sm" variant="outline-success">
-          + <i class="fa fa-smile-o emoji"></i>
-        </b-button>
-      </div>
+    <b-col cols="9" md="10">
+      <b-container class="h-100">
+        <b-row style="height: inherit;">
+          <b-col align-self="start" cols="12" class="mb-3">
+            <p>
+              <small>{{post.text}}</small>
+            </p>
+          </b-col>
+          <b-col align-self="end" cols="6" class="text-left">
+            <b-button :id="'edit' + post['.key'].slice(-5) +'Button'" href="#" size="sm"
+                      variant="outline-secondary"
+                      class="edit-post-button">
+              <icon name="pencil-alt"/>
+            </b-button>
+            <b-tooltip :target="'edit' + post['.key'].slice(-5) +'Button'" title="Make a change"/>
+          </b-col>
+          <b-col align-self="end" cols="6" class="text-right">
+            <ul class="list-inline">
+              <li class="list-inline-item">💡</li>
+              <li class="list-inline-item">❤</li>
+              <li class="list-inline-item">👎</li>
+              <li class="list-inline-item">👍</li>
+              <li class="list-inline-item">👌</li>
+            </ul>
+            <b-button size="sm" variant="outline-success">
+              <span class="emoji">❤</span>️ 3
+            </b-button>
+            <b-button size="sm" variant="outline-success" class="active-reaction">
+              <span class="emoji">👌️</span>️ 1
+            </b-button>
+            <b-button size="sm" variant="outline-success">
+              + <i class="fa fa-smile-o emoji"></i>
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-container>
     </b-col>
   </b-row>
 </template>
@@ -91,5 +97,8 @@
   .post {
     border: 1px solid lightgrey;
     border-radius: 5px;
+    .edit-post-button {
+      line-height: 1;
+    }
   }
 </style>

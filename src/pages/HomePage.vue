6af2e3187e4cdcefc70137b4c183c87ae3aebe33
Helen <kirren@live.com>
2018-05-29 12:14:47 +0300

    Add forum list and forum list items and replace thread lists with them

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
     <h1>Welcome to the Forum</h1>
-    <ThreadList :threads="threads"/>
+    <ForumList :forums="forums"/>
   </div>
 </template>
 
 <script>
   import sourceData from '@/data'
-  import ThreadList from '@/components/ThreadList'
+  import ForumList from '@/components/ForumList'
 
   export default {
     components: {
-      ThreadList
+      ForumList
     },
     data () {
       return {
-        threads: Object.values(sourceData.threads),
+        forums: Object.values(sourceData.forums),
         users: sourceData.users,
         posts: sourceData.posts
       }

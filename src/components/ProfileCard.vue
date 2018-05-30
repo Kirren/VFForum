82cf73dd2cc583c23554ff8d3b3510c81361cb03
Helen <kirren@live.com>
2018-05-30 12:10:35 +0300

    Move profile card to a separate component + add mobile view

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,100 @@
+<template>
+  <b-col cols="12" lg="6">
+    <b-row class="text-center d-lg-none">
+      <b-col>
+        <p>
+          <b-img fluid
+                 :src="user.avatar"
+                 alt=""/>
+        </p>
+      </b-col>
+      <b-col>
+        <h1>{{user.username}}</h1>
+        <p>{{user.name}}</p>
+        <p>
+          <span v-if="user.bio">{{user.bio}}</span>
+          <span v-else>No bio specified.</span>
+        </p>
+        <span class="online">{{user.username}} is online</span>
+        <div class="stats">
+          <small>{{userPostsCount}} posts</small>
+          <small>{{userThreadsCount}} threads</small>
+        </div>
+        <hr>
+        <p v-if="user.website" class="text-center">
+          <icon name="globe"/>
+          <a :href="user.website">{{user.website}}</a>
+        </p>
+        <p class="text-muted text-center">
+          <small>Member since june 2003, last visited 4 hours ago</small>
+        </p>
+
+        <div class="text-center">
+          <hr>
+          <b-button href="edit-profile.html"
+                    variant="success" size="sm">
+            Edit Profile
+          </b-button>
+        </div>
+      </b-col>
+    </b-row>
+    <b-card class="text-center d-none d-lg-block">
+      <p>
+        <b-img fluid
+               :src="user.avatar"
+               alt=""/>
+      </p>
+      <h1>{{user.username}}</h1>
+      <p>{{user.name}}</p>
+      <p>
+        <span v-if="user.bio">{{user.bio}}</span>
+        <span v-else>No bio specified.</span>
+      </p>
+      <span class="online">{{user.username}} is online</span>
+      <div class="stats">
+        <small>{{userPostsCount}} posts</small>
+        <small>{{userThreadsCount}} threads</small>
+      </div>
+      <hr>
+      <p v-if="user.website" class="text-center">
+        <icon name="globe"/>
+        <a :href="user.website">{{user.website}}</a>
+      </p>
+      <p class="text-muted text-center">
+        Member since june 2003, last visited 4 hours ago
+      </p>
+
+      <div class="text-center">
+        <hr>
+        <b-button href="edit-profile.html"
+                  variant="success" size="sm">
+          Edit Profile
+        </b-button>
+      </div>
+    </b-card>
+  </b-col>
+</template>
+
+<script>
+  export default {
+    name: 'ProfileCard',
+    props: {
+      user: {
+        required: true,
+        type: Object
+      },
+      userPostsCount: {
+        required: true,
+        type: Number
+      },
+      userThreadsCount: {
+        required: true,
+        type: Number
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>

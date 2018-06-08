656cfbe012912253221b1664aba40e8e3110ae93
Helen <kirren@live.com>
2018-06-08 16:42:50 +0300

    Add thread to database on create

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,27 +3,35 @@ import firebase from 'firebase'
 export default {
   createThread ({commit, state, dispatch}, {title, text, forumId}) {
     return new Promise((resolve, reject) => {
-      const threadId = 'someFictionalId' + Math.random()
+      const threadId = firebase.database().ref('threads').push().key
+      const postId = firebase.database().ref('posts').push().key
       const userId = state.authId
       const publishedAt = Math.floor(Date.now() / 1000)
 
-      const thread = {
-        '.key': threadId,
-        title,
-        forumId,
-        publishedAt,
-        userId
-      }
+      const thread = { title, forumId, publishedAt, userId, firstPostId: postId, posts: {} }
+      thread.posts[postId] = postId
+      const post = { text, publishedAt, threadId, userId }
 
-      commit('setThread', {thread, threadId})
-      commit('appendThreadToForum', {parentId: forumId, childId: threadId})
-      commit('appendThreadToUser', {parentId: userId, childId: threadId})
+      const updates = {}
+      updates[`threads/${threadId}`] = thread
+      updates[`forums/${forumId}/threads/${threadId}`] = threadId
+      updates[`users/${userId}/threads/${threadId}`] = threadId
 
-      dispatch('createPost', {text, threadId})
-        .then(post => {
-          commit('setThread', {threadId, thread: {...thread, firstPostId: post['.key']}})
+      updates[`posts/${postId}`] = post
+      updates[`users/${userId}/posts/${postId}`] = postId
+
+      firebase.database().ref().update(updates)
+        .then(() => {
+          commit('setData', {resource: 'threads', item: thread, id: threadId})
+          commit('appendThreadToForum', {parentId: forumId, childId: threadId})
+          commit('appendThreadToUser', {parentId: userId, childId: threadId})
+
+          commit('setData', {resource: 'posts', item: post, id: postId})
+          commit('appendPostToThread', {parentId: post.threadId, childId: postId})
+          commit('appendPostToUser', {parentId: post.userId, childId: postId})
+
+          resolve(state.threads[threadId])
         })
-      resolve(state.threads[threadId])
     })
   },
   updateThread ({commit, state, dispatch}, {title, text, id}) {

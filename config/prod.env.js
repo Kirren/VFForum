98a06abb9a235d27f75608bff52a0634cea20888
Helen <kirren@live.com>
2018-06-11 11:39:05 +0300

    Move firebase credentials to a .env file

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,10 +1,4 @@
 'use strict'
 module.exports = {
-  FIREBASE_NODE_ENV: '"production"',
-  FIREBASE_API_KEY: '"AIzaSyDZod57OLrTilKKhqf3Ulvhqfc2AtR-9_E"',
-  FIREBASE_AUTH_DOMAIN: '"vfforum-3eda6.firebaseapp.com"',
-  FIREBASE_DATABASE_URL: '"https://vfforum-3eda6.firebaseio.com"',
-  FIREBASE_PROJECT_ID: '"vfforum-3eda6"',
-  FIREBASE_STORAGE_BUCKET: '"vfforum-3eda6.appspot.com"',
-  FIREBASE_MESSAGING_SENDER_ID: '"124241809010"'
+  NODE_ENV: '"production"'
 }

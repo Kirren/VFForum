cb369c32f9aba31372788ae50cfaeb570fc32f8f
Helen <kirren@live.com>
2018-06-11 11:27:41 +0300

    Exchange firebase config values with environmental variables

--- src/main.js
+++ src/main.js
@@ -21,12 +21,12 @@ Vue.component('icon', Icon)
 Vue.config.productionTip = false
 
 const config = {
-  apiKey: 'AIzaSyDZod57OLrTilKKhqf3Ulvhqfc2AtR-9_E',
-  authDomain: 'vfforum-3eda6.firebaseapp.com',
-  databaseURL: 'https://vfforum-3eda6.firebaseio.com',
-  projectId: 'vfforum-3eda6',
-  storageBucket: 'vfforum-3eda6.appspot.com',
-  messagingSenderId: '124241809010'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 }
 firebase.initializeApp(config)
 

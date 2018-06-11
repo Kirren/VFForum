8e0c86f8859592bf42c7b05a67d9f6a80c644baa
Helen <kirren@live.com>
2018-06-11 11:23:43 +0300

    Add firebase credentials to prod.env.js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,10 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  FIREBASE_NODE_ENV: '"production"',
+  FIREBASE_API_KEY: 'AIzaSyDZod57OLrTilKKhqf3Ulvhqfc2AtR-9_E',
+  FIREBASE_AUTH_DOMAIN: 'vfforum-3eda6.firebaseapp.com',
+  FIREBASE_DATABASE_URL: 'https://vfforum-3eda6.firebaseio.com',
+  FIREBASE_PROJECT_ID: 'vfforum-3eda6',
+  FIREBASE_STORAGE_BUCKET: 'vfforum-3eda6.appspot.com',
+  FIREBASE_MESSAGING_SENDER_ID: '124241809010'
 }

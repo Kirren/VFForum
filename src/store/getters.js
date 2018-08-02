607bcbebc0b5c95331bc28fd4ad513de2abfc8e4
Helen <kirren@live.com>
2018-08-02 11:41:31 +0300

    Remove moved

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,18 +1,1 @@
-import { countObjectLength } from '../helpers'
-
-export default {
-  authUser (state) {
-    return state.authId ? state.users[state.authId] : null
-  },
-  userThreadsCount: state => id => countObjectLength(state.users[id].threads),
-  userPosts: state => id => {
-    const user = state.users[id]
-    if (user.posts) {
-      return Object.values(state.posts)
-        .filter(post => post.userId === id)
-    }
-    return []
-  },
-  userPostsCount: state => id => countObjectLength(state.users[id].posts),
-  threadContributorsCount: state => id => countObjectLength(state.threads[id].contributors) - 1
-}
+export default {}
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,24 +1,8 @@
 import Vue from 'vue'
-import { makeAppendChildToParentMutation } from './storeHelpers'
 
 export default {
-  setAuthId (state, id) {
-    state.authId = id
-  },
-  setUser (state, {user, userId}) {
-    Vue.set(state.users, userId, user)
-  },
-  setPost (state, {post, postId}) {
-    Vue.set(state.posts, postId, post)
-  },
   setData (state, {resource, item, id}) {
     item['.key'] = id
     Vue.set(state[resource], id, item)
-  },
-  setUnsubscribeAuthObserver (state, unsubscribe) {
-    state.unsubscribeAuthObserver = unsubscribe
-  },
-
-  appendPostToUser: makeAppendChildToParentMutation({parent: 'users', child: 'posts'}),
-  appendThreadToUser: makeAppendChildToParentMutation({parent: 'users', child: 'threads'})
+  }
 }
